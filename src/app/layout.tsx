f7fc2c7a186e901212920632d24d50e8121b9a6a
eduarchive88 <eduarchive88@github.com>
2026-04-22 09:12:32 +0900

    feat: 푸터 추가 (만든 사람, 카카오톡 오픈채팅, 블로그 링크)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,30 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <footer className="mt-auto py-4 text-center text-sm text-gray-400 border-t border-gray-700 bg-gray-900">
+          만든 사람: 경기도 지구과학 교사 뀨짱&nbsp;&nbsp;|&nbsp;&nbsp;
+          문의:{" "}
+          <a
+            href="https://open.kakao.com/o/s7hVU65h"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline hover:text-gray-200"
+          >
+            카카오톡 오픈채팅
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;블로그:{" "}
+          <a
+            href="https://eduarchive.tistory.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline hover:text-gray-200"
+          >
+            뀨짱쌤의 교육자료 아카이브
+          </a>
+        </footer>
+      </body>
     </html>
   );
 }
